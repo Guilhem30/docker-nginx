a8dae6cb5124c29e816caee23cb90432a3449983
Guilhem Berna <guilhem.berna@gmail.com>
2014-12-11 15:43:53 +0100

    moved seafile.conf to sites-available

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -9,19 +9,20 @@ if [ -f /etc/nginx/conf.d/"${nginxConfFile}" ]
 then
 	echo "Configuration Found, Loading it"
 else
-	cd /etc/nginx/conf.d/
+	cd /etc/nginx/sites-available/
 	echo "No configuration found, Creating it from the template"
 	mv /root/seafile.conf ./"${nginxConfFile}"
-        mkdir $sslDir
+    mkdir $sslDir
 	export RANDFILE="${sslDir}"/.rnd
-        openssl genrsa -out "${sslDir}"/$DOMAIN_NAME.key 2048
-        openssl req -new -x509 -key "${sslDir}"/$DOMAIN_NAME.key -out "${sslDir}"/$DOMAIN_NAME.crt -days 1825 -subj "/C=FR/ST=France/L=Paris/O=phosphore/CN=$DOMAIN_NAME" 
+    openssl genrsa -out "${sslDir}"/$DOMAIN_NAME.key 2048
+    openssl req -new -x509 -key "${sslDir}"/$DOMAIN_NAME.key -out "${sslDir}"/$DOMAIN_NAME.crt -days 1825 -subj "/C=FR/ST=France/L=Paris/O=phosphore/CN=$DOMAIN_NAME" 
 	sed -i "s/#SEAFILE IP#/$SEAFILE_IP/g" "${nginxConfFile}"
 	sed -i "s/#SEAHUB PORT#/$SEAHUB_PORT/g" "${nginxConfFile}"
 	sed -i "s/#FILESERVER PORT#/$FILESERVER_PORT/g" "${nginxConfFile}"
 	sed -i "s/#DOMAIN NAME#/$DOMAIN_NAME/g" "${nginxConfFile}"
 	sed -i 's|#SSL CERTIFICATE#|'$sslDir/$DOMAIN_NAME'.crt|g' "${nginxConfFile}"
-        sed -i 's|#SSL KEY#|'$sslDir/$DOMAIN_NAME'.key|g' "${nginxConfFile}"
-
+    sed -i 's|#SSL KEY#|'$sslDir/$DOMAIN_NAME'.key|g' "${nginxConfFile}"
+	rm /etc/nginx/sites-enabled/default
+	ln -s /etc/nginx/sites-available/seafile.conf /etc/nginx/sites-enabled/seafile.conf
 fi
 
